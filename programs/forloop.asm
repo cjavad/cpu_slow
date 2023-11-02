56a39a9d7f73bf07b5365aa82ae87c45d1714ab8
Javad Asgari Shafique <me@javad.sh>
2023-11-02 19:40:02 +0100

    Add forloop.asm

---
+++ programs/forloop.asm
@@ -0,0 +1,9 @@
+SET r0 0    ; For loop counter
+
+main:
+STORE r0 0  ; Store counter in memory
+STORE r0 r0 ; Store rolling sum in memory at i index
+ADD r0 r0 1 ; increment with 1
+CMP r0 50   ; compare with 50
+JLE main     ; if equal, jump to main
+HALT        ; end program
