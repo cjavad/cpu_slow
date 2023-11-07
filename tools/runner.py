d15757293d3d4c9efc88a34180e81d99deffa177
Javad Asgari Shafique <me@javad.sh>
2023-11-07 20:21:21 +0100

    Make runner run on both examples

--- tools/runner.py
+++ tools/runner.py
@@ -44,35 +44,37 @@ image = [
     0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, 255, 255, 255,   0,   0,   0,   0,   0,   0
   ]]
 
-image = image[1]
-image.extend([0] * (2**16 - len(image)))
+def run(image):
+    image.extend([0] * (2**16 - len(image)))
 
-# Write to mem.hex as hex
-with open('mem.hex', 'wb') as f:
-    for instr in image:
-        f.write(f"{instr:08x}\n".encode('utf-8'))
+    # Write to mem.hex as hex
+    with open('mem.hex', 'wb') as f:
+        for instr in image:
+            f.write(f"{instr:08x}\n".encode('utf-8'))
 
-# Build the verilog iverilog -o sim ../generated/CPUTop.v simulate.v
-subprocess.run(['iverilog', '-o', 'sim', '../generated/CPUTop.v', 'simulate.v'])
+    # Build the verilog iverilog -o sim ../generated/CPUTop.v simulate.v
+    subprocess.run(['iverilog', '-o', 'sim', '../generated/CPUTop.v', 'simulate.v'], stdout=subprocess.PIPE)
+    # Run the simulation
+    subprocess.run(['./sim'], stdout=subprocess.PIPE)
 
-# Run the simulation
-subprocess.run(['./sim'])
+    # Read the output (out.hex) strip // comments
+    with open('out.hex', 'r') as f:
+        out = f.readlines()
 
-# Read the output (out.hex) strip // comments
-with open('out.hex', 'r') as f:
-    out = f.readlines()
+    def print_image(image):
+        for i, p in enumerate(image):
+            if i % 20 == 0:
+                print("")
+            print(str(p).ljust(5), end=" ")
 
-def print_image(image):
-    for i, p in enumerate(image):
-         if i % 20 == 0:
-             print("")
-         print(str(p).ljust(5), end=" ")
+    memory = [int(line.split('//')[0], 16) for line in out if line.strip() != '' and not line.startswith('//')]
+    input_picture = memory[0:400]
+    output_picture = memory[400:800]
 
-memory = [int(line.split('//')[0], 16) for line in out if line.strip() != '' and not line.startswith('//')]
-input_picture = memory[0:400]
-output_picture = memory[400:800]
+    print("\n")
+    print("Input:")
+    print_image(input_picture)
+    print("\n\nOutput:")
+    print_image(output_picture)
 
-print("Input:")
-print_image(input_picture)
-print("\n\nOutput:")
-print_image(output_picture)
+[run(i) for i in image]
